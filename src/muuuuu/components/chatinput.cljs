26be01c81408d2140d1cca72578f8b9b36da9cbb
Jelle Akkerman <jelle@jelleakkerman.nl>
2014-03-03 21:33:19 +0100

    starting nickname autocomplete

--- src/muuuuu/components/chatinput.cljs
+++ src/muuuuu/components/chatinput.cljs
@@ -2,6 +2,9 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [clojure.string]
+            [goog.events :as events]
+            [goog.events.KeyHandler]
+            [goog.events.KeyCodes]
             [sablono.core :as html :refer-macros [html]]
             [muuuuu.utils :refer [guid get-active-rooms current-room]]))
 
@@ -30,27 +33,36 @@
         ;(prn (get-in res [:body :message])))))
 
 (defn send-message
-  [e state owner]
+  [e state roomname owner]
   (let [[text text-node] (value-from-node owner "yourmessage")]
     (when text
-      ;(om/transact! (current-room (:rooms @state))
-        ;(fn [msgs] (conj msgs {:sender "you" :content text})))
+      (om/transact! state [:rooms roomname :msgs]
+        (fn [msgs] (conj msgs {:sender "You" :content text :msg-type "self"})))
       (clear-nodes! text-node))
     false))
 
 (defn init [state owner]
   (if (> (count (get-active-rooms (:rooms state))) 0)
-  (om/component
-    (html [:div.chatinput
-            [:form {:onSubmit #(send-message % state owner)}
-              [:div {:className
-                  (str "name" (if (:bright (:color (second (first
-                  (filter (fn [r] (= (:inviewport (second r)) true)) (:rooms state))
-                                   ))) true) "" " bright"))}
-                (:yourname state)]
-              [:input#yourmsg.yourmessage {:type "text" :ref "yourmessage"
-                                         :placeholder "Your Message"}]
-              [:input {:type "submit" :value "Send!"}]
-          ]]))
+  (let [current (current-room (:rooms state)) roomname (str (first current))]
+    (reify
+      om/IDidMount
+      (did-mount [_]
+        (let [keyboard (events.KeyHandler. (by-class "name"))]
+          (event/listen keyboard
+                        "key"
+                        (fn [e] (when (= (.-keyCode e) events.KeyCodes/TAB)
+                                 (do (prn "ENTEERRU!"))))))
+      )
+      om/IRender
+      (render [_]
+        (html [:div.chatinput
+                [:form {:onSubmit #(send-message % state roomname owner)}
+                  [:div {:className
+                      (str "name" (if (:bright (:color (second current)) true) "" " bright"))}
+                    (:yourname state)]
+                  [:input#yourmsg.yourmessage {:type "text" :ref "yourmessage"
+                                             :placeholder "Your Message"}]
+                  [:input {:type "submit" :value "Send!"}]
+              ]]))))
   (om/component
     (html [:div ""]))))
