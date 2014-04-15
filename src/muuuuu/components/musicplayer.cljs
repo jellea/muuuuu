cd236286faaf2894ec5655d6df6ad11f503659ec
Jelle Akkerman <jelle@jelleakkerman.nl>
2014-04-15 19:17:03 +0200

    Adding play pause to music playback

--- src/muuuuu/components/musicplayer.cljs
+++ src/muuuuu/components/musicplayer.cljs
@@ -4,12 +4,37 @@
             [sablono.core :as html :refer-macros [html]]
             [om.dom :as dom :include-macros true]))
 
-(defn init [{:keys [tracknumber tracktitle artist data-url is-playing] :as app} owner]
+
+
+(defn toggle-play-pause [state]
+  (om/transact! state
+    #(assoc % :is-playing (not (:is-playing %)))))
+
+(defn next-track [state]
+  (prn @state)
+)
+
+(defn empty-playlist [state]
+  (prn @state)
+)
+
+(defn love-track [state]
+  (prn @state)
+)
+
+(defn init [{:keys [tracknumber tracktitle artist
+                    data-url playlist is-playing] :as state} owner]
   (reify
     om/IDidUpdate
       (did-update [_ _ _]
         (if is-playing
-          (.play (. js/document (getElementById "audioplayer")) nil))
+          (.play (. js/document (getElementById "audioplayer")) nil)
+          (.pause (. js/document (getElementById "audioplayer")) nil))
+      )
+    om/IDidMount
+      (did-mount [_]
+        (.addEventListener (. js/document (getElementById "audioplayer"))
+          "ended" #(next-track state))
       )
     om/IRender
       (render [_]
@@ -17,4 +42,5 @@
               [:div.text.hide (str tracknumber ". " artist " - " tracktitle)]
               [:div.heart.hide "<3"]
               [:audio#audioplayer {:src data-url}]
-              [:div.playbtn ">"]]))))
+              [:div.playbtn {:onClick #(toggle-play-pause state)}
+                (if is-playing ">" "||")]]))))
