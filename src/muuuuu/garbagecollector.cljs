ed0213b91b8f453c2d4594910cfb3ad9b17331cb
Jelle Akkerman <jelle@jelleakkerman.nl>
2014-04-19 14:04:46 +0200

    adding a elegant garbage/message collector

---
+++ src/muuuuu/garbagecollector.cljs
@@ -0,0 +1,21 @@
+(ns muuuuu.garbagecollector
+  (:require [om.core :as om :include-macros true]
+            [cljs.core.async :refer [<! chan timeout]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(defn prunemsgs
+  "Limits the number of messages in rooms on the state to nummsgs"
+  [state nummsgs]
+  (om/transact state [:rooms]
+               (fn [rooms]
+                 (apply hash-map (flatten
+                   (map
+                     (fn [room] (list (room 0)
+                                      (update-in (room 1) [:msgs] #(take nummsgs %))))
+                     rooms))))))
+
+(defn interval [state]
+  (go
+    (while true
+      (<! (timeout 10000))
+      (prunemsgs state 20))))
